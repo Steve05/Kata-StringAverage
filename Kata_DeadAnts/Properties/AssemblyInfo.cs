3b73264be7ed8651f83f85dcafd0dd76949ec038
Steve05 <steve.yeh05@gmail.com>
2017-09-27 09:35:55 +0800

    Initial commit

--- Kata_DeadAnts/Properties/AssemblyInfo.cs
+++ Kata_DeadAnts/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("StringAverage")]
+[assembly: AssemblyTitle("Kata_DeadAnts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StringAverage")]
+[assembly: AssemblyProduct("Kata_DeadAnts")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("6b994e53-d09a-45b4-bc07-8b5d421d8827")]
+[assembly: Guid("ea9f4598-d0a4-45b8-919e-d730a7680df6")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
